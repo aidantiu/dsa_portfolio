151414feae42da889975b3caa37f86498dbb98f3
ymnwri <tiuprof77@gmail.com>
2024-12-09 21:50:24 +0800

    feat: create profile section

--- portfolio/app/routes/index.py
+++ portfolio/app/routes/index.py
@@ -10,4 +10,17 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+
+
+    # List of profiles
+    profiles = [
+        {"image_path": "images/aidan.jpg", "name": "Aidan Tiu", "role": "Project Manager"},
+        {"image_path": "images/renz.png", "name": "Renz Tyrone Arcilla", "role": "Back-end Developer"},
+        {"image_path": "images/earl.jpg", "name": "Earl Clyde Banez", "role": "UI/UX Designer"},
+        {"image_path": "images/denn.jpg", "name": "Denn Adrian Capus", "role": "Front-end Developer"},
+        {"image_path": "images/roman.jpg", "name": "Roman Joseph Gallardo", "role": "Front-end Developer"},
+        {"image_path": "images/railey.jpg", "name": "Railey Guinto", "role": "UI/UX Designer"},
+        {"image_path": "images/arvie.jpg", "name": "Arvie Lastra", "role": "Back-end Developer"},
+    ]
+    
+    return render_template('index.html', title='Home', profiles=profiles)

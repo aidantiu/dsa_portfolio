e23e7b33ab77bc65c06c6b88b9f6e5ccba7f3aa0
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2025-01-12 12:08:56 +0800

    Add back to top button functionality and styles.

--- portfolio/app/static/scripts/index.js
+++ portfolio/app/static/scripts/index.js
@@ -1,3 +1,23 @@
-//
-// This file is used to load all the scripts that are used in the index.html file.
-//
+// Script for back to top button
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopBtn = document.getElementById('back-to-top');
+
+    window.onscroll = function() {
+        requestAnimationFrame(function() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                backToTopBtn.style.opacity = "1";
+                backToTopBtn.style.visibility = "visible";
+            } else {
+                backToTopBtn.style.opacity = "0";
+                backToTopBtn.style.visibility = "hidden";
+            }
+        });
+    };
+
+    backToTopBtn.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
